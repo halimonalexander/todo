74f066ced13c55cd8444b30a62f153154c77f44b
A.Halimon <a@halimon.me>
2024-05-01 21:27:45 +0300

    add authorization

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,10 +3,22 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/halimonalexander/todo"
 	"github.com/halimonalexander/todo/pkg/repository"
 	"os"
+	"time"
 )
+
+const (
+	tokenTtl   = 12 * time.Hour
+	signingKey = "ejL38gjk#s!"
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -26,4 +38,23 @@ func (s *AuthService) generatePasswordHash(password string) string {
 	hash.Write([]byte(password))
 
 	return fmt.Sprintf("%x", hash.Sum([]byte(os.Getenv("SALT"))))
+}
+
+func (s *AuthService) GenerateToken(username string, password string) (string, error) {
+	password = s.generatePasswordHash(password)
+
+	user, err := s.repo.GetUser(username, password)
+	if err != nil {
+		return "", err
+	}
+
+	var token = jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTtl).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
